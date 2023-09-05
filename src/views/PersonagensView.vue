48c6dc3dbcc9ab511fddcf9fe2af6910deda6335
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-09-04 21:42:43 -0300

    função axios para pegar os personagens

--- src/views/PersonagensView.vue
+++ src/views/PersonagensView.vue
@@ -1,1 +1,16 @@
 <template><h1>Personagens</h1></template>
+<script setup>
+import axios from "axios";
+
+// Post - Envia uma requisição
+// Get - Pega os dados
+console.log("GEt - Faz uma requisição");
+
+axios({
+  method: "get",
+  url: "https://hp-api.onrender.com/api/characters",
+}).then(function (response) {
+  let personagens = response.data.slice(0, 10);
+  console.log(personagens[1].name);
+});
+</script>
