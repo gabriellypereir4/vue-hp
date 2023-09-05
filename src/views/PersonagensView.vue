6b89a240545298c2a0e2decb9bd6b303c37a09c3
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-09-04 22:00:35 -0300

    mostrando personagem na tela

--- src/views/PersonagensView.vue
+++ src/views/PersonagensView.vue
@@ -1,6 +1,14 @@
-<template><h1>Personagens</h1></template>
+<template>
+  <h1>Nome(name): {{ personagem.name }}</h1>
+  <h1>Casa(house): {{ personagem.house }}</h1>
+  <h1>Patrono(patronus): {{ personagem.patronus }}</h1>
+  <img :src="personagem.image" />
+</template>
 <script setup>
 import axios from "axios";
+import { ref } from "vue";
+
+const personagem = ref();
 
 // Post - Envia uma requisição
 // Get - Pega os dados
@@ -10,7 +18,7 @@ axios({
   method: "get",
   url: "https://hp-api.onrender.com/api/characters",
 }).then(function (response) {
-  let personagens = response.data.slice(0, 10);
-  console.log(personagens[1].name);
+  let personagens = response.data; //.slice(0, 300);
+  personagem.value = personagens[14];
 });
 </script>
